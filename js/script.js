6f5588834c9969272c1dc5429b68bc4baa32678a
124msalvani <124msalvani@frhsd.com>
2024-04-24 09:36:43 -0400

    Translation still not working, set up dropdown and can get input value

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-document.getElementById("submit").addEventListener("click", initialize);
+var translated;
+var dict = {
+    "Home": {
+      pt: "Início"
+    },
+    "Download plugin": {
+       pt: "Descarregar plugin",
+       en: "Download plugin"
+    }
+  };
+var translator = $('body').translate({lang: "en", t: dict}); //use English
 
+document.getElementById("submit").addEventListener("click", onClick);
+function onClick() {
+    inputText = document.getElementById("input").value;
+    console.log(inputText);
+    translator.lang("fr");
+    console.log(translated);
+    document.getElementById('output').innerHTML = translated;
 
-
-function initialize() {
-    var text = document.getElementById("text").value;
-    console.log(text)
-    google.language.translate(text, 'es', 'en', function(result) {
-        var translated = document.getElementById("output");
-        if (result.translation) {
-            translated.innerHTML = result.translation;
-        }
-    });
+    
 }
-
-
