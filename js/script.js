2889cfb300c810b96e52da23e8d27ad226859b0d
124msalvani <124msalvani@frhsd.com>
2024-04-29 08:37:14 -0400

    Working translation website, need to style

--- js/script.js
+++ js/script.js
@@ -1,22 +1,70 @@
-var translated;
-var dict = {
-    "Home": {
-      pt: "Início"
-    },
-    "Download plugin": {
-       pt: "Descarregar plugin",
-       en: "Download plugin"
+const dropdowns = document.querySelectorAll(".dropdown-container"),
+  inputLanguageDropdown = document.querySelector("#input-language"),
+  outputLanguageDropdown = document.querySelector("#output-language");
+
+function populateDropdown(dropdown, options) {
+  dropdown.querySelector("ul").innerHTML = "";
+  options.forEach((option) => {
+    const li = document.createElement("li");
+    const title = option.name + " (" + option.native + ")";
+    li.innerHTML = title;
+    li.dataset.value = option.code;
+    li.classList.add("option");
+    dropdown.querySelector("ul").appendChild(li);
+  });
+}
+
+populateDropdown(outputLanguageDropdown, languages);
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    dropdown.classList.toggle("active");
+  });
+
+  dropdown.querySelectorAll(".option").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      //remove active class from current dropdowns
+      dropdown.querySelectorAll(".option").forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+      const selected = dropdown.querySelector(".selected");
+      selected.innerHTML = item.innerHTML;
+      selected.dataset.value = item.dataset.value;
+      translate();
+    });
+  });
+});
+document.addEventListener("click", (e) => {
+  dropdowns.forEach((dropdown) => {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove("active");
     }
-  };
-var translator = $('body').translate({lang: "en", t: dict}); //use English
+  });
+});
 
-document.getElementById("submit").addEventListener("click", onClick);
-function onClick() {
-    inputText = document.getElementById("input").value;
-    console.log(inputText);
-    translator.lang("fr");
-    console.log(translated);
-    document.getElementById('output').innerHTML = translated;
 
+  inputLanguage = inputLanguageDropdown.querySelector(".selected"),
+  outputLanguage = outputLanguageDropdown.querySelector(".selected"),
+  inputTextElem = document.querySelector("#input-text"),
+  outputTextElem = document.querySelector("#output-text");
+
+
+
+function translate() {
+  const inputText = inputTextElem.value;
+  const inputLanguage =
+    inputLanguageDropdown.querySelector(".selected").dataset.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
+    inputText,
+  )}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      outputTextElem.value = json[0].map((item) => item[0]).join("");
+    })
     
 }
+
